ea941925da2adf4cf44b1b2a58a44c7cf1f62cfb
DovahYol <DovahYol@live.com>
2023-05-31 02:06:12 +0800

    add finetuning command

--- run_sentiment.sh
+++ run_sentiment.sh
@@ -1,3 +1,4 @@
 # python classifier.py --option finetune --use_gpu --batch_size 96
 # python classifier.py --option pretrain --use_gpu --batch_size 64 --lr 1e-3
 python multitask_classifier.py --option pretrain --use_gpu --batch_size 64 --lr 1e-3
+python multitask_classifier.py --option finetune --use_gpu --batch_size 64
